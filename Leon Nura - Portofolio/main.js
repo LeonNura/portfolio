7fe7e17e8834b16b68b9bf2bf11819f9367908ef
LeonNura <leonnura7369.0@gmail.com>
2022-12-01 23:47:15 +0100

    update

---
+++ Leon Nura - Portofolio/main.js
@@ -0,0 +1,28 @@
+var btn = document.getElementById('btn');
+
+btn.addEventListener('click', function(e){
+    // e.preventDefault();
+
+    // var name = document.getElementById('name').value;
+    // var email = document.getElementById('email').value;
+    // var message = document.getElementById('message').value;
+    // var subject = document.getElementById('subject').value;
+    // var body = 'Name: ' + name + '<br/> Email: ' + email + '</br> Subject: ' + subject + '<br/> Message: ' + message;
+
+
+    // Email.send({
+    //     // SecureToken: "e848a6f9-8bec-4717-a815-4bec0a1929f2",
+
+    //     Host : "smtp.elasticemail.com",
+    //     Username : "leonnura7369.0@gmail.com",
+    //     Password : "lena73loni69",
+    //     To : 'leonnura7369.0@gmail.com',
+    //     From : email,
+    //     Subject : subject,
+    //     Body : body
+    // }).then(
+    //   message => alert(message)
+    // );
+
+    alert("Message sent successfully");
+})
